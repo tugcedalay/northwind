8738499e60b186b58e2fa49ba34fcbc22e1fcc97
Tugce Dalay <tgcdalay@gmail.com>
2024-10-20 01:33:18 +0300

    category creat

--- northwind/src/main/java/com/tobeto/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/com/tobeto/dataAccess/abstracts/ProductDao.java
@@ -3,6 +3,7 @@ package com.tobeto.dataAccess.abstracts;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.tobeto.core.utilities.results.DataResult;
 import com.tobeto.core.utilities.results.Result;
@@ -11,6 +12,29 @@ import com.tobeto.entities.concretes.Product;
 //crud operasyonları yapmamızı sağlar 
 //generic yapıda jpa repository her nesne için çalışır
 //product icin calısıcam pk alanımın veri türü integer
+//getBy ilgili kolona gider ve ver kosulu yazar
 public interface ProductDao extends JpaRepository<Product, Integer>{
-
+      Product getByProdductName(String productName);
+      
+      Product getByProductNameAndCategoryId(String productName, int categoryId);
+      
+      
+      //iki kolonu da getirebilecegi icin or kullanırken list kullandim
+      
+      List<Product> getByProductNameOrCategoryId(String productName, int categoryId);
+      
+      //select* from products where product_name = abc or category_id = 1
+      //select* from products where category_id in(1, 2, 3, 4) 
+      
+      List<Product> getByCategoryIdIn(List<Integer> categories);
+      
+      List<Product> getByProductNameContains(String productName);
+      
+      List<Product> getByProductNameStartsWith(String productName);
+      
+      //queryde entityden yazdık, parametre iki noktalı
+      @Query("From Product where productName =:productName and categoryId=:categoryId")
+      List<Product> getByNameAndCategory(String productName, int categoryId);
+      
+      //select * from products where product_name = abc and categoryId=abc
 }
--- northwind/src/main/java/com/tobeto/entities/concretes/category.java
+++ northwind/src/main/java/com/tobeto/entities/concretes/category.java
@@ -1,5 +1,31 @@
 package com.tobeto.entities.concretes;
 
-public class category {
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categories")
+@Entity
+public class category {
+	
+  @Id
+  @Column(name = "category_id")	
+  private int categoryId;
+  
+  @Column(name = "category_name")
+  private String categoryName;
+  
+  @OneToMany(mappedBy = "categories")
+  private List<Product> products;
 }
